b7c205f42c75fe371284a6de7662404a394824b4
yanghao <yanghao8079@qq.com>
2020-02-25 15:41:22 +0800

    add jcart store back

---
+++ jcart-store-back/src/test/java/com/yh/jcartstoreback/JcartStoreBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yh.jcartstoreback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartStoreBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
