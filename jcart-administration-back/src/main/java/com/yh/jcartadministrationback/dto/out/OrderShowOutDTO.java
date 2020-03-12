8ada48c61537e4ead46a9bbb190a6f5c0ddf0aef
yanghao <yanghao8079@qq.com>
2020-03-12 14:12:10 +0800

    修改后台 admin  OrderShowOutDTO

--- jcart-administration-back/src/main/java/com/yh/jcartadministrationback/dto/out/OrderShowOutDTO.java
+++ jcart-administration-back/src/main/java/com/yh/jcartadministrationback/dto/out/OrderShowOutDTO.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class OrderShowOutDTO {
     private Long orderId;
     private Integer customerId;
-    private  String customerName;
+    private String customerName;
     private Byte status;
     private Double totalPrice;
     private Integer rewordPoints;
@@ -25,6 +25,7 @@ public class OrderShowOutDTO {
     private String invoiceAddress;
     private Double invoicePrice;
     private String comment;
+    private List<OrderProductVO> orderProducts;
 
     public Long getOrderId() {
         return orderId;
@@ -40,14 +41,6 @@ public class OrderShowOutDTO {
 
     public void setCustomerId(Integer customerId) {
         this.customerId = customerId;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
     }
 
     public Byte getStatus() {
@@ -146,7 +139,19 @@ public class OrderShowOutDTO {
         this.comment = comment;
     }
 
-    public void setOrderProducts(List<OrderProductVO> orderProductVOS) {
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public List<OrderProductVO> getOrderProducts() {
+        return orderProducts;
+    }
+
+    public void setOrderProducts(List<OrderProductVO> orderProducts) {
+        this.orderProducts = orderProducts;
     }
 }
-
