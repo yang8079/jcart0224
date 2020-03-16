3a0e59a6a9f5ee29319419db51ddcdb2dd437f20
yanghao <yanghao8079@qq.com>
2020-03-16 13:43:47 +0800

    修改前端 admin 商品列表编辑页面跳转

--- jcart-administration-front/rout-page/router.js
+++ jcart-administration-front/rout-page/router.js
@@ -1,9 +1,8 @@
 const routes = [
     { path: '/product/search', component: ProductSearchRoutePage },
-    { path: '/customer/search', component: CustomerSearchRoutePage },
-    // { path: '/product/update/:productId', component: ProductUpdateRoutePage },
-
-     
+    { path: '/product/update/:productId', component: ProductUpdateRoutePage },
+    
+    { path: '/customer/search', component: CustomerSearchRoutePage },  
     { path: '/order/search', component: OrderSearchRoutePage },
     { path: '/return/search', component: ReturnSearchRoutePage },
     { path: '/administrator/index', component: AdministratorIndexRoutePage }
