8adf609e9a6d80bfe7fb06fb3ed04ec37db4a95e
yanghao <yanghao8079@qq.com>
2020-03-20 15:26:38 +0800

    amdin mq

---
+++ jcart-administration-back/src/main/java/com/yh/jcartadministrationback/mq/EmailEvent.java
@@ -0,0 +1,37 @@
+package com.yh.jcartadministrationback.mq;
+
+/**
+ * @Description
+ * @Author Yh
+ * @Date 2020-03-20 15:18
+ */
+public class EmailEvent {
+
+    private String toEmail;
+    private String title;
+    private String content;
+
+    public String getToEmail() {
+        return toEmail;
+    }
+
+    public void setToEmail(String toEmail) {
+        this.toEmail = toEmail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ jcart-support-back/src/main/java/com/yh/jcartsupportback/JcartSupportBackApplication.java
@@ -0,0 +1,13 @@
+package com.yh.jcartsupportback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcartSupportBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcartSupportBackApplication.class, args);
+    }
+
+}
